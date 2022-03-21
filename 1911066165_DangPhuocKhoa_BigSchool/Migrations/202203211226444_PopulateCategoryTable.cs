ef37c9365435c3b9ef35859a5c361451de9217d0
KhoaTG <khoa.dp2k@gmail.com>
2022-03-21 21:51:55 +0700

    Commit vào lúc 22h52 ngày 21/03/2022

---
+++ 1911066165_DangPhuocKhoa_BigSchool/Migrations/202203211226444_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+namespace _1911066165_DangPhuocKhoa_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            //Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (1,'Development')");
+            //Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (2,'Business')");
+            //Sql("INSERT INTO CATEGORIES (ID,NAME) VALUES (3,'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ 1911066165_DangPhuocKhoa_BigSchool/Migrations/202203211316194_AddAttendance.cs
@@ -0,0 +1,16 @@
+namespace _1911066165_DangPhuocKhoa_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttendance : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
